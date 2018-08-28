205272cfbb1d8800c7f3217d800e21b9e3e1aa43
Dani Schuhman <dani8439@aol.com>
2018-08-28 19:22:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-
       erb :index
     end
 
     post '/teams' do
+      puts params
     end
 
 
