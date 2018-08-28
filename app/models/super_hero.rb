dc6a95ced6c404f9a7035240a3bcbda44c9734a7
Dani Schuhman <dani8439@aol.com>
2018-08-28 19:30:20 +0000

    automatically backed up by learn

--- app/models/super_hero.rb
+++ app/models/super_hero.rb
@@ -1,2 +1,11 @@
-class SuperHero 
+class SuperHero
+  attr_reader :name, :motto, :bio
+  SUPERHEROES = []
+
+  def initialize(args)
+    @name = args[:name]
+    @motto = args[:motto]
+    @bio = args[:bio]
+    SUPERHEROES << self 
+  end
 end
